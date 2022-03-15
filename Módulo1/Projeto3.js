35c475539c47bf874ffaf346599d2b8486158f1b
santos95mat <santos95.mat@gmail.com>
2022-03-15 20:01:54 -0300

    Update Projeto3.js

--- Módulo1/Projeto3.js
+++ Módulo1/Projeto3.js
@@ -1,27 +1,53 @@
 const prompt = require('prompt-sync')();
 
-let res = [];
+let i_dia = 1;
+let dia_mes = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+let mes = ['janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+let i_mes = 0;
+
+let dia_semana = ['Sabado', 'Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira'];
+let i_semana = 0;
+
+let p = ['manhã', 'tarde', 'noite'];
+let i_p = 0;
+
+let pessoa = ["Claudio", "Beatriz"];
+let player = {};
+
+
+function dia(){
+  i_dia++;
+  i_semana++;
+  
+  if(i_dia > dia_mes[i_mes]){
+    i_mes++;
+    i_dia = 1;
+  }
+  if(i_semana > 6){
+    i_semana = 0;
+  }
+}
+
+function periodo(){
+  if(i_mes > 11){
+    console.log("O Ano acabou!")
+  }else{ 
+    console.log(`${dia_semana[i_semana]} - ${i_dia} de ${mes[i_mes]} de 2022`);
+    console.log(p[i_p]);
+    console.log();
+  }
+  
+  i_p++;
+
+  if(i_p>2){
+    dia();
+    i_p=0;
+  } 
+}
+
+player.nome = prompt("Qual o seu nome: ");
+player.sexo = prompt("Você é homem ou mulher: ");
 
 console.clear();
-console.log("09 de Julho de 1984!");
-console.log("Seu despertador toca, são 08:30 da manhã, você se levanta, toma seu remédio e se olha npo espelho.");
-
-prompt("Precione ENTER para continuar...");
-
-console.log("Você sai do seu quarto, se depara com seu pai, que te chama para tomar um café da manhã");
-
-prompt("Precione ENTER para continuar...");
-
-console.log("Você se senta a mesa, esta muito nevorso, pois hoje você vai apresentar o projeto do seu jogo para uma companhia.")
-
-prompt("Precione ENTER para continuar...");
-
-console.log("Seu jogo é baseado em um livro onde você decidade a aventura do personagem principal, seu pai em um tom de humor, lhe perguna")
-
-prompt("Precione ENTER para continuar...");
-
-console.log("Qual sereal você gostaria de tomar hoje?");
-console.log();
-consolole.log("[1] Sugar Puffs")
-consolole.log("[2] Sucrilhos")
-res = prompt('');
+periodo();
