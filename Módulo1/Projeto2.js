0183ea4d6bbb6a092e09b137b6ebfad77a4d4bcd
Matheus Rodrigues Santos <98832555+santos95mat@users.noreply.github.com>
2022-03-15 19:48:01 -0300

    Add files via upload

---
+++ Módulo1/Projeto2.js
@@ -0,0 +1,112 @@
+const prompt = require('prompt-sync')();
+const colors = require('colors');
+
+function jokenPo(){
+  console.clear();
+  
+  let jogo = ["pedra", "papel", "tesoura"];
+  let p = ""; // player
+  let m = ""; // máquina
+  let r = 0; // numero de rodadas
+  let placar = [0, 0]; // placar
+  let table = {}; // tabela do placar
+  
+  function enter(){
+    console.log();
+    prompt('press ENTER para continuar');
+    console.clear();
+  }
+  
+  function rodadas(){
+    console.clear();
+    console.log("JO".red + "KEN".blue + "PO".red);
+    console.log();
+    r = prompt('Escolha quantidade de rodadas: ');
+    if(isNaN(r) || r<=0){
+      rodadas();
+    }
+    console.clear();
+  }
+  
+  function game(){   
+    for(let i=1; i<=r; i++){
+      
+      let choice = false;
+      while(choice == false){
+        console.clear();
+        console.log("JO".red + "KEN".blue + "PO".red + ` - Rodada ${i} de ${r}` );
+        console.log();
+        
+        function score(s1, s2){
+          this.Você = s1;
+          this.Máquina = s2
+        }
+        table.Pontos = new score(placar[0], placar[1]);   
+        console.table(table);
+
+        console.log();
+        p = prompt("[pedra, papel ou tesoura]:").toLowerCase();
+        if(p=='pedra' || p=='papel' || p=='tesoura'){
+          choice = true;
+        }
+      }
+      m = jogo[Math.floor(Math.random() * jogo.length)];
+
+      console.log();
+      console.log('Maquina:',m);
+      console.log();
+  
+      if((p=='pedra' && m=='tesoura') || (p=='tesoura' && m=='papel') || (p=='papel' && m=='pedra')){
+        console.log(`Você venceu a rodada ${i}`);
+        placar[0] += 1;
+      }
+      else if(m==p){
+        console.log(`Vocês empataram a rodada ${i}`);
+      }
+      else{
+        console.log(`Máquina venceu a rodada ${i}`);
+        placar[1] += 1;
+      }
+      enter();
+    }
+  }
+
+  function end(){
+    console.clear();
+    console.log("JO".red + "KEN".blue + "PO".red);
+    console.log();
+    
+    function score(s1, s2){
+      this.Você = s1;
+      this.Máquina = s2
+    }
+    table.Pontos = new score(placar[0], placar[1]);   
+    console.table(table);
+    
+    console.log();
+
+    if(placar[0] > placar[1]){
+      console.log('Você ganhou o jogo!');
+    }else if(placar[1] > placar[0]){
+      console.log('Você perdeu o jogo!')
+    }else{
+      console.log('Você empatou o jogo!')
+    }
+
+    console.log();
+    
+    let i = "";
+    i = prompt("jogar de novo?[s/n]").toLowerCase();
+    if(i==='s'){
+      jokenPo();
+    }else{
+      console.clear();
+    }
+  }
+
+  rodadas();
+  game();
+  end();
+}
+
+jokenPo();
---
+++ Módulo1/Projeto3.js
@@ -0,0 +1,27 @@
+const prompt = require('prompt-sync')();
+
+let res = [];
+
+console.clear();
+console.log("09 de Julho de 1984!");
+console.log("Seu despertador toca, são 08:30 da manhã, você se levanta, toma seu remédio e se olha npo espelho.");
+
+prompt("Precione ENTER para continuar...");
+
+console.log("Você sai do seu quarto, se depara com seu pai, que te chama para tomar um café da manhã");
+
+prompt("Precione ENTER para continuar...");
+
+console.log("Você se senta a mesa, esta muito nevorso, pois hoje você vai apresentar o projeto do seu jogo para uma companhia.")
+
+prompt("Precione ENTER para continuar...");
+
+console.log("Seu jogo é baseado em um livro onde você decidade a aventura do personagem principal, seu pai em um tom de humor, lhe perguna")
+
+prompt("Precione ENTER para continuar...");
+
+console.log("Qual sereal você gostaria de tomar hoje?");
+console.log();
+consolole.log("[1] Sugar Puffs")
+consolole.log("[2] Sucrilhos")
+res = prompt('');
